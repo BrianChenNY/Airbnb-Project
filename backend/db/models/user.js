916749a245d770e801bcf5f322c4eea452fc6601
ZiwenZhang <a60595677@gmail.com>
2024-09-18 16:35:37 -0400

    Add User model scope for user security

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -24,6 +24,59 @@
 //   return User;
 // };
 
+// 'use strict';
+
+// const { Model, Validator } = require('sequelize');
+
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+
+
+//   User.init(
+//     {
+//       username: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//         validate: {
+//           len: [4, 30],
+//           isNotEmail(value) {
+//             if (Validator.isEmail(value)) {
+//               throw new Error('Cannot be an email.');
+//             }
+//           },
+//         },
+//       },
+//       email: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//         validate: {
+//           len: [3, 256],
+//           isEmail: true,
+//         },
+//       },
+//       hashedPassword: {
+//         type: DataTypes.STRING.BINARY,
+//         allowNull: false,
+//         validate: {
+//           len: [60, 60],
+//         },
+//       },
+//     },
+//     {
+//       sequelize,
+//       modelName: 'User',
+//     }
+//   );
+//   return User;
+// };
+
+
 'use strict';
 
 const { Model, Validator } = require('sequelize');
@@ -34,7 +87,6 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-
 
   User.init(
     {
@@ -71,6 +123,11 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'User',
+      defaultScope: {
+        attributes: {
+          exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+        },
+      },
     }
   );
   return User;
