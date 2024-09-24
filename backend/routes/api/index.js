309d283a76aeaba046c9a766defb7b683d1dd8aa
ZiwenZhang <a60595677@gmail.com>
2024-09-24 17:22:12 -0400

    added spotimages api/ added delete a spot image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,8 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spot.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./booking.js');
+const spotImagesRouter = require('./spotImages.js');
+
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 router.use(restoreUser);
@@ -16,6 +18,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
+router.use('/spot-images', spotImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
