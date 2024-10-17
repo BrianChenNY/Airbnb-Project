314bc5a6644d8406d25096010bbc8dc365a7c384
BrianChenNY <shunchao.chen@outlook.com>
2024-10-16 23:01:27 -0400

    Auth Me P1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import configureStore from './store/store';
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'; // <-- ADD THIS LINE
 
 const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+	restoreCSRF();
+
+	window.csrfFetch = csrfFetch;
+	window.store = store;
+  window.sessionActions = sessionActions; // <-- ADD THIS LINE
+}
+
+//Create a variable to access your store and expose it on the window. It should not be exposed in production; 
+//make sure this is only set in development.
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
